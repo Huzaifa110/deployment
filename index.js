39ec5adb293fee33710cc7594e14866cf2797b0c
Muhammad Huzaifa <114142659+Huzaifa110@users.noreply.github.com>
2024-05-02 21:25:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -19,18 +19,8 @@ app.use('/api', require("./routes/login"));
 app.use('/api', require("./routes/register"));
 app.use('/api', require("./routes/dishes"));
 
-//app.use(express.static(path.join(__dirname,'./frontend/build/')))
-//app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname,'./','frontend','build','index.html')))
-
-
-// Serve static files from the frontend build directory
-app.use(express.static(path.join(__dirname, './frontend/build/')));
-
-// Serve index.html for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './frontend', 'build', 'index.html'));
-});
-
+app.use(express.static(path.join(__dirname,'./frontend/build/')))
+app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname,'./','frontend','build','index.html')))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
