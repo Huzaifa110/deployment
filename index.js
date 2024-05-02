14692e857417b81d0a5482ea90c0f390ff88b4fd
Muhammad Huzaifa <114142659+Huzaifa110@users.noreply.github.com>
2024-05-02 20:52:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use('/api', require("./routes/register"));
 app.use('/api', require("./routes/dishes"));
 
 app.use(express.static(path.join(__dirname,'./frontend/build/')))
-app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname,'./','frontend','buildcd','index.html')))
+app.get('/',(req,res)=>res.sendFile(path.resolve(__dirname,'./','frontend','build','index.html')))
 
 
 app.listen(port, () => {
